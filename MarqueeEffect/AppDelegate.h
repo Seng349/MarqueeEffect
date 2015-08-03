9d80313348926d0f0889678752dc01a154c30567
Seng349 <luckSeng@gmail.com>
2015-08-03 14:24:36 +0800

    Initial Commit

---
+++ MarqueeEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarqueeEffect
+//
+//  Created by ZhengWenQiang on 8/3/15.
+//  Copyright © 2015 seng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarqueeEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarqueeEffect
+//
+//  Created by ZhengWenQiang on 8/3/15.
+//  Copyright © 2015 seng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
