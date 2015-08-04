7c380a982596102c46f8a0cb2c379aed7ba90025
Seng349 <luckSeng@gmail.com>
2015-08-04 13:24:39 +0800

    Marquee Effect

---
+++ MarqueeEffect/MarqueeLayer.h
@@ -0,0 +1,13 @@
+//
+//  MarqueeLayer.h
+//  MarqueeEffect
+//
+//  Created by ZhengWenQiang on 8/3/15.
+//  Copyright © 2015 seng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MarqueeLayer : CAShapeLayer
+
+@end
---
+++ MarqueeEffect/MarqueeLayer.m
@@ -0,0 +1,13 @@
+//
+//  MarqueeLayer.m
+//  MarqueeEffect
+//
+//  Created by ZhengWenQiang on 8/3/15.
+//  Copyright © 2015 seng. All rights reserved.
+//
+
+#import "MarqueeLayer.h"
+
+@implementation MarqueeLayer
+
+@end
